b89a7e3558d8245b923a90ff5c56082bb16f2442
William Horn <wbhorn@alaska.edu>
2018-05-22 11:41:10 -0800

    Ability to conditionaly run download test

--- asf_granule_util/tests/main.py
+++ asf_granule_util/tests/main.py
@@ -1,6 +1,16 @@
 import unittest
+import sys
+
+try:
+    shouldnt_dl = sys.argv[1]
+except:
+    from test_download import TestDownload
+else:
+    sys.argv.remove(shouldnt_dl)
+
+
 from test_granules import TestSentinelGranule
-from test_download import TestDownload
+from test_pairs import TestPairs
 
 if __name__ == "__main__":
     unittest.main()
