6acc34815dffdaaa1cf12e6b660237b50ebf9622
William Horn <wbhorn@alaska.edu>
2018-05-22 12:54:42 -0800

    Combine test/prod upload scripts into one

--- asf_granule_util/upload.sh
+++ asf_granule_util/upload.sh
@@ -1,15 +1,34 @@
 #! /usr/bin/env bash
 
-read  -n 1 -p "Be sure to run a test upload before production upload (Enter)" mainmenuinput
+if [ "$1" = "test" ]
+then
+    mkdir dist-old
+    mv dist/* dist-old/
+    echo "Last version: "
+    cat version.txt
+    echo "Enter new version: "
+    read version
+    echo $version > version.txt
 
-git add version.txt
-git commit -m "Uploaded version $version to PyPi"
+    echo python setup.py sdist
+    python2 setup.py sdist
+    echo python setup.py bdist_wheel --universal
+    python2 setup.py bdist_wheel --universal
 
-echo python setup.py sdist
-python2 setup.py sdist
-echo python setup.py bdist_wheel --universal
-python2 setup.py bdist_wheel --universal
+    twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+elif [ "$1" = "prod" ]
+then
+    read  -n 1 -p "Be sure to run a test upload before production upload (Enter)" mainmenuinput
 
-twine upload dist/*
+    git add version.txt
+    git commit -m "Uploaded version $version to PyPi"
 
+    echo python setup.py sdist
+    python2 setup.py sdist
+    echo python setup.py bdist_wheel --universal
+    python2 setup.py bdist_wheel --universal
 
+    twine upload dist/*
+else
+    echo "test-upload.sh [test|prod]"
+fi
