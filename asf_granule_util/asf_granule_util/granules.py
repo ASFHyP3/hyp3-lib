8c85cce05d29b5b0de6c41e625c5b0e44d5055f9
William Horn <wbhorn@alaska.edu>
2018-05-22 11:07:36 -0800

    Put sentinel regex in class as static member

--- asf_granule_util/asf_granule_util/granules.py
+++ asf_granule_util/asf_granule_util/granules.py
@@ -4,29 +4,14 @@ import re
 import datetime
 
 
-# From: https://earth.esa.int/web/sentinel/user-guides/sentinel-1-sar/naming-conventions
-sentinel_pattern = re.compile(r"""
-        (S1[AB])_           # Mission ID
-        (IW|EW|WV|S[1-6])_  # Mode/Beam
-        (GRD|SLC|OCN)       # Product Type
-        ([FHM_])_           # Resolution
-        ([12])              # Processing Level
-        ([SA])              # Product Class
-        (SH|SV|DH|DV)_      # Polarization
-        (\d{8})T(\d{6})_    # Start (Date)T(Time)
-        (\d{8})T(\d{6})_    # End (Date)T(Time)
-        (\d{6})_            # Absolut Orbit Number
-        ([0-9A-F]{6})_      # Missin Data Take ID
-        ([0-9A-F]{4})       # Product Unique ID
-""", re.X)
-
-
 class Granule(object):
     def __init__(self, pattern, granule_string, attributes):
         m = re.search(pattern, granule_string)
         if m is None:
             raise InvalidGranuleException(
-                "The string given does not describe a valid granule!", granule_string)
+                "The string given does not describe a valid granule!",
+                granule_string
+            )
 
         g = m.groups()
         for i, attribute in enumerate(attributes):
@@ -43,24 +28,48 @@ class Granule(object):
         return str(self.to_str())
 
     def __repr__(self):
-        return self.__str__()
-
+        return 'Granule({g})'.format(g=self.str())
 
 
 class SentinelGranule(Granule):
+    # From: https://earth.esa.int/web/sentinel/user-guides/sentinel-1-sar/naming-conventions
+    pattern = re.compile(r"""
+            (S1[AB])_           # Mission ID
+            (IW|EW|WV|S[1-6])_  # Mode/Beam
+            (GRD|SLC|OCN)       # Product Type
+            ([FHM_])_           # Resolution
+            ([12])              # Processing Level
+            ([SA])              # Product Class
+            (SH|SV|DH|DV)_      # Polarization
+            (\d{8})T(\d{6})_    # Start (Date)T(Time)
+            (\d{8})T(\d{6})_    # End (Date)T(Time)
+            (\d{6})_            # Absolut Orbit Number
+            ([0-9A-F]{6})_      # Missin Data Take ID
+            ([0-9A-F]{4})       # Product Unique ID
+    """, re.X)
+
     @staticmethod
     def is_valid(possible_granule_string):
-        return re.match(sentinel_pattern, possible_granule_string)
+        return re.match(SentinelGranule.pattern, possible_granule_string)
 
     def __init__(self, granule_string):
         length = len(granule_string)
 
         if length != 67:
             raise InvalidGranuleException(
-                "Granule string is too {}! Must be 67 characters".format("short" if length < 67 else "long"), granule_string)
+                "Granule string is too {}! Must be 67 characters".format(
+                    "short" if length < 67 else "long"
+                ),
+                granule_string
+            )
 
         attributes = self.get_attributes()
-        super(SentinelGranule, self).__init__(sentinel_pattern, granule_string, attributes)
+
+        super(SentinelGranule, self).__init__(
+            SentinelGranule.pattern,
+            granule_string,
+            attributes
+        )
 
     def to_str(self):
         return "_".join([
@@ -104,4 +113,3 @@ class SentinelGranule(Granule):
     def get_date(self, date_str, dtype='str'):
         """Returns datetime object from date_str formatted '%Y%m%d%H%M%S'"""
         return datetime.datetime.strptime(date_str, '%Y%m%d%H%M%S')
-
