df5823524b1a334bdaf8cfbd346daa3aebb4e8bd
Joseph H Kennedy <me@jhkennedy.org>
2020-02-05 09:52:10 -0900

    Remove unescessary and large top comment blocks

--- hyp3lib/execute.py
+++ hyp3lib/execute.py
@@ -1,36 +1,9 @@
 #!/usr/bin/env python
-# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
-###############################################################################
-# execute.py
-#
-# Project:  APD 
-# Purpose:  Execute a commnand 
-#  
-# Author:   Tom Logan
-#
-# Issues/Caveats:
-#
-###############################################################################
-# Copyright (c) 2017, Alaska Satellite Facility
-# 
-# This library is free software; you can redistribute it and/or
-# modify it under the terms of the GNU Library General Public
-# License as published by the Free Software Foundation; either
-# version 2 of the License, or (at your option) any later version.
-# 
-# This library is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# Library General Public License for more details.
-# 
-# You should have received a copy of the GNU Library General Public
-# License along with this library; if not, write to the
-# Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-# Boston, MA 02111-1307, USA.
-###############################################################################
+
 import os
 import subprocess
 import logging
+
 
 def execute(cmd, expected=None, logfile=None, uselogging=False):
 
--- hyp3lib/makeColorPhase.py
+++ hyp3lib/makeColorPhase.py
@@ -1,35 +1,6 @@
 #!/usr/bin/env python
 """Create a colorize phase file from a phase geotiff"""
 
-# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
-###############################################################################
-# makeColorPhase.py
-#
-# Project:  HYP3 InSAR
-# Purpose:  Create a colorized image from phase data
-#  
-# Author:   Tom Logan
-#
-# Issues/Caveats:
-#
-###############################################################################
-# Copyright (c) 2018, Alaska Satellite Facility
-# 
-# This library is free software; you can redistribute it and/or
-# modify it under the terms of the GNU Library General Public
-# License as published by the Free Software Foundation; either
-# version 2 of the License, or (at your option) any later version.
-# 
-# This library is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# Library General Public License for more details.
-# 
-# You should have received a copy of the GNU Library General Public
-# License along with this library; if not, write to the
-# Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-# Boston, MA 02111-1307, USA.
-###############################################################################
 import os
 import sys
 import math
