6a56c03fb46de334127bdbe30a7f7731676ef7e3
Joseph H Kennedy <me@jhkennedy.org>
2020-02-06 15:44:28 -0900

    Use tox for muli-python environment testing
    Minor changes to the tests for python 27

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,6 +7,6 @@ _HERE = os.path.dirname(__file__)
 
 @pytest.fixture(scope='session')
 def safe_data(tmpdir_factory):
-    safe_dir = tmpdir_factory.mktemp('safe_data').join('test.SAFE')
+    safe_dir = str(tmpdir_factory.mktemp('safe_data').join('test.SAFE'))
     shutil.copytree(os.path.join(_HERE, 'data', 'test.SAFE'), safe_dir)
     return safe_dir
--- tests/test_get_bounding.py
+++ tests/test_get_bounding.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 
 from hyp3lib import get_bounding
@@ -55,7 +56,7 @@ def test_annotation_file_sample():
 
 
 def test_annotation_file(safe_data):
-    with open(safe_data.join('annotation', 'test-swath-1.xml')) as f:
+    with open(os.path.join(safe_data, 'annotation', 'test-swath-1.xml')) as f:
         box_xml = f.read()
 
     truth = (37.11964, 38.78037, -112.54442, -111.184078)  # lat_min, lat_max, lon_min, lon_max
