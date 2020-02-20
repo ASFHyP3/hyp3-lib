d8ba7e398c60b60ccf45ebc5e91ac200a07ca18c
Cameron Showalter <cjshowalter@alaska.edu>
2020-02-20 14:42:17 -0900

    Made fileList optional, made ncFile accept both filepath, and stream, returns list of geotiff paths for loading into other functions.

--- src/time_series2geotiff.py
+++ src/time_series2geotiff.py
@@ -7,57 +7,52 @@ import sys
 from osgeo import gdal, ogr, osr
 import netCDF4 as nc
 from asf_geometry import data2geotiff
-from asf_time_series import nc2meta, getNetcdfGranule
+from asf_time_series import ncStream2meta, getNetcdfGranule
+from argparse_helpers import file_exists, dir_exists_create
 
 
-def time_series2data(ncFile, granuleList=None):
+# ncFilePath = str path to nc.Dataset, or Dataset itself
+# outDir = str path
+# granuleList = list[str]
+def time_series2geotiff(ncFile, outDir, granuleList=None):
+  # Doublecheck outDir, incase called from another script:
+  outDir = dir_exists_create(outDir)
+  # If you pass a path, vs passing the file itself:
+  if isinstance(ncFile, str):
+    dataset = nc.Dataset(ncFile, 'r')
+    opened_file = True
+  else:
+    dataset = ncFile
+    opened_file = False
+
   if granuleList == None:
-    granuleList = getNetcdfGranule(ncFile)
-  ### Read granule list
-  image = ncFile.variables['image'][:]
-  granules = ncFile.variables['granule'][:]
-  granules = list(nc.chartostring(granules, encoding='utf-8'))
-
-  geotiff_list = []
-  for granule in granuleList:
-    t = granules.index(granule)
-    data = image[t]
-    geotiff_list.append(data)
-  print("geotiff_list")
-  print(geotiff_list)
-  return geotiff_list
-
-
-
-
-def time_series2geotiff(ncFilePath, listFile, outDir):
-  ### Read granule list
-  granuleList = [line.rstrip() for line in open(listFile)]
+    granuleList = getNetcdfGranule(dataset)
 
   ### Read time series data
-  print('Reading time series file ({0}) ...'.format(ncFile))
-  dataset = nc.Dataset(ncFilePath, 'r')
-  SOMETHING = time_series2data(dataset, granuleList=granuleList)
-  dataset.close()
-
+  images = dataset.variables['image'][:]
+  
   ### Read time series metadata and
-  meta = nc2meta(ncFilePath)
+  meta = ncStream2meta(dataset)
   spatialRef = osr.SpatialReference()
   spatialRef.ImportFromEPSG(meta['epsg'])
   geoTrans = ( meta['minX'], meta['pixelSize'], 0, meta['maxY'], 0, -meta['pixelSize'])
 
-  ### Go through granules
-  if not os.path.exists(outDir):
-    os.makedirs(outDir)
-  for line in lines:
+  geotiff_paths = []
+  ### Go through granuleList
+  for granule in granuleList:
     ## Determine index of the granule in the time series
-    t = granules.index(line)
-    data = image[t,:,:]
-    outFile = os.path.join(outDir, line+'_time_series.tif')
+    t = granuleList.index(granule)
+    data = images[t,:,:]
+    outFile = os.path.join(outDir, granule+'_time_series.tif')
+    geotiff_paths.append(outFile)
 
     ## Save time series layers into GeoTIFF files
     print('Saving result to GeoTIFF file ({0})'.format(outFile))
     data2geotiff(data, geoTrans, spatialRef, 'FLOAT', 0, outFile)
+  # If you opened it, you close it:
+  if opened_file:
+    dataset.close()
+  return geotiff_paths
 
 
 if __name__ == '__main__':
@@ -65,20 +60,20 @@ if __name__ == '__main__':
   parser = argparse.ArgumentParser(prog='time_series2geotiff',
     description='extracts time series layers and stores them in GeoTIFF format',
     formatter_class=RawTextHelpFormatter)
-  parser.add_argument('inFile', metavar='<input file>',
+  parser.add_argument('inFile', metavar='<input file>', action="store", type=file_exists, 
     help='name of the netCDF time series file')
-  parser.add_argument('listFile', metavar='<granule list>',
-    help='name of the granule list file')
-  parser.add_argument('outDir', metavar='<output directory>',
+  parser.add_argument('outDir', metavar='<output directory>', action="store", type=dir_exists_create, 
     help='name of the output directory')
+  parser.add_argument('--listFile', metavar='<granule list>', action="store", type=file_exists,
+    help='name of the granule list file')
   if len(sys.argv) == 1:
     parser.print_help()
     sys.exit(1)
   args = parser.parse_args()
 
-  if not os.path.exists(args.inFile):
-    print('netCDF file (%s) does not exist!' % args.inFile)
-    sys.exit(1)
+  ### Read granule list
+  if args.listFile != None:
+    listFile = [line.rstrip() for line in open(args.listFile)]
 
-  # time_series2geotiff(args.inFile, args.listFile, args.outDir)
-  time_series2data(args.inFile)
+  time_series2geotiff(args.inFile, args.outDir, granuleList=args.listFile)
+
