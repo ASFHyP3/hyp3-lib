f8f14a249ee43435f3f69b818f0564c4670abe1f
Joseph H Kennedy <me@jhkennedy.org>
2020-03-12 16:36:24 -0800

    Move mkdir_p from hyp3proclib to hyp3lib

--- hyp3lib/file_subroutines.py
+++ hyp3lib/file_subroutines.py
@@ -1,18 +1,20 @@
 from __future__ import print_function, absolute_import, division, unicode_literals
 
+import errno
 import os
 import re
 from hyp3lib.execute import execute
 import zipfile
 
-#
-#  Given a CSV file, download granules and unzip them,
-#  removing zip files as we go.  Note, this will unzip
-#  and REMOVE ALL ZIP FILES in the current directory.
-#
-def prepare_files(csvFile):
 
-    cmd = "get_asf.py %s" % csvFile
+def prepare_files(csv_file):
+    """Download granules and unzip granules
+
+    Given a CSV file of granule names, download the granules and unzip them,
+    removing the zip files as we go. Note: This will unzip and REMOVE ALL ZIP
+    FILES in the current directory.
+    """
+    cmd = "get_asf.py %s" % csv_file
     execute(cmd)
     os.rmdir("download")
     for myfile in os.listdir("."):
@@ -22,12 +24,12 @@ def prepare_files(csvFile):
             zip_ref.close()    
             os.remove(myfile)
 
-#
-# Return lists of file names and file dates, sorted by date.
-# Includes all SAFE directories found in the current directory.
-#
-def get_file_list():
 
+def get_file_list():
+    """
+    Return a list of file names and file dates, including all SAFE
+    directories, found in the current directory, sorted by date.
+    """
     files = []
     filenames = []
     filedates = []
@@ -36,17 +38,30 @@ def get_file_list():
     i = 0
     for myfile in os.listdir("."):
         if ".SAFE" in myfile and os.path.isdir(myfile):
-            t = re.split('_+',myfile)
-            m = [myfile,t[4][0:15]]
+            t = re.split('_+', myfile)
+            m = [myfile, t[4][0:15]]
             files.append(m)
-            i = i+1
+            i += 1
 
     print('Found %s files to process' % i)
-    files.sort(key = lambda row: row[1])
+    files.sort(key=lambda row: row[1])
     print(files)
 
     for i in range(len(files)): 
         filenames.append(files[i][0])
         filedates.append(files[i][1])
 
-    return(filenames,filedates)
+    return filenames, filedates
+
+
+def mkdir_p(path):
+    """
+    Make parent directories as needed and no error if existing. Works like `mkdir -p`.
+    """
+    try:
+        os.makedirs(path)
+    except OSError as exc:  # Python >2.5
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            raise
