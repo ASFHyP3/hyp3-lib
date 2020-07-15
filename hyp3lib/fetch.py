d6838519fccd30eb4de43e08c3e04e0ac767d85e
Joseph H Kennedy <me@jhkennedy.org>
2020-07-15 15:27:35 -0800

    Fetch Docs

--- hyp3lib/fetch.py
+++ hyp3lib/fetch.py
@@ -8,10 +8,23 @@ from requests.adapters import HTTPAdapter
 from urllib3.util.retry import Retry
 
 
-def download_file(url, directory=None, headers=None, chunk_size=None, retries=3, backoff_factor=10):
+def download_file(url: str, directory: str = '.', headers=None, chunk_size=None, retries=3, backoff_factor=10):
+    """Download a file
+
+    Args:
+        url: URL of the file to download
+        directory: Directory location to place files into
+        headers: Dictionary of headers to add to the the download request
+        chunk_size: Size to chunk the download into
+        retries: Number of retry's to attempt
+        backoff_factor: Factor for calculating time between retries
+
+    Returns:
+        download_path: The path to the downloaded file
+    """
     logging.info(f'Downloading {url}')
 
-    local_filename = os.path.join(directory, url.split("/")[-1])
+    download_path = os.path.join(directory, url.split("/")[-1])
 
     session = requests.Session()
     retry = Retry(
@@ -25,8 +38,8 @@ def download_file(url, directory=None, headers=None, chunk_size=None, retries=3,
 
     with requests.get(url, headers=headers, stream=True) as r:
         r.raise_for_status()
-        with open(local_filename, "wb") as f:
+        with open(download_path, "wb") as f:
             for chunk in r.iter_content(chunk_size=chunk_size):
                 if chunk:
                     f.write(chunk)
-    return local_filename
+    return download_path
