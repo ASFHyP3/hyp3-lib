2351b51d4a1eb2163baec1f3a1a053e756e7113b
Andrew Johnston <asjohnston@alaska.edu>
2020-08-12 15:55:39 -0800

    individual test functions

--- tests/test_get_dem.py
+++ tests/test_get_dem.py
@@ -7,11 +7,13 @@ from hyp3lib import DemError
 from hyp3lib.get_dem import get_dem, get_best_dem
 
 
-def test_get_best_dem():
+def test_get_best_dem_no_coverage():
     # atlantic ocean, south of western africa
     with pytest.raises(DemError):
         get_best_dem(y_min=0, y_max=1, x_min=0, x_max=1)
 
+
+def test_get_best_dem_ned13():
     # utah, western united states
     (name, projection, tile_list, wkt_list) = get_best_dem(y_min=38.2, y_max=38.8, x_min=-110.8, x_max=-110.2)
     assert name == 'NED13'
@@ -19,6 +21,8 @@ def test_get_best_dem():
     assert tile_list == ['n39w111']
     assert wkt_list == ['POLYGON ((-111.000556 39.000556,-109.999444888884 39.000556,-109.999444888884 37.9994448888845,-111.000556 37.9994448888845,-111.000556 39.000556))']
 
+
+def test_get_best_dem_srtmgl1():
     # democratic republic of the congo, southern africa
     (name, projection, tile_list, wkt_list) = get_best_dem(y_min=-6.8, y_max=-6.2, x_min=27.2, x_max=27.8)
     assert name == 'SRTMGL1'
@@ -26,6 +30,8 @@ def test_get_best_dem():
     assert tile_list == ['S07E027']
     assert wkt_list == ['POLYGON ((26.999861 -5.999861,28.0001387777858 -5.999861,28.0001387777858 -7.00013877778578,26.999861 -7.00013877778578,26.999861 -5.999861))']
 
+
+def test_get_best_dem_ned2():
     # alaska
     (name, projection, tile_list, wkt_list) = get_best_dem(y_min=67.9, y_max=68.1, x_min=-155.6, x_max=-155.4)
     assert name == 'NED2'
@@ -36,11 +42,15 @@ def test_get_best_dem():
        'POLYGON ((-156.003333 69.003333,-154.996666333325 69.003333,-154.996666333325 67.9966663333253,-156.003333 67.9966663333253,-156.003333 69.003333))',
     ]
 
-    # northern russia, just missing coverage threshold
+
+def test_get_best_dem_just_missing_coverage_threshold():
+    # northern russia
     with pytest.raises(DemError):
         get_best_dem(y_min=59.976, y_max=60.1, x_min=99.5, x_max=100.5)
 
-    # northern russia, just passing coverage threshold
+
+def test_get_best_dem_just_passing_coverage_threshold():
+    # northern russia
     (name, projection, tile_list, wkt_list) = get_best_dem(y_min=59.975, y_max=60.1, x_min=99.5, x_max=100.5)
     assert name == 'SRTMGL1'
     assert projection == 4326
@@ -51,10 +61,12 @@ def test_get_best_dem():
     ]
 
 
-def test_get_dem(tmp_path, golden_dem):
+def test_get_dem_no_coverage():
     with pytest.raises(DemError):
         get_dem(y_min=0, y_max=1, x_min=0, x_max=1, outfile='dem.tif', post=30.0)
 
+
+def test_get_dem_srtmgl1(tmp_path, golden_dem):
     chdir(tmp_path)
     output_file = tmp_path / 'dem.tif'
     name = get_dem(y_min=-6.8, y_max=-6.79, x_min=27.79, x_max=27.8, outfile=str(output_file), post=30.0)
