6a298b444c18cf44fade9d705837a8d38842e0dc
Joseph H Kennedy <me@jhkennedy.org>
2020-09-22 20:44:43 -0800

    Bring in v2 get_download_url for sentinel-1 scenes

---
+++ hyp3lib/scene.py
@@ -0,0 +1,12 @@
+"""Tools for working with Sentinel-1 scenes"""
+
+SENTINEL_DISTRIBUTION_URL = 'https://d2jcx4uuy4zbnt.cloudfront.net'
+
+
+def get_download_url(scene):
+    mission = scene[0] + scene[2]
+    product_type = scene[7:10]
+    if product_type == 'GRD':
+        product_type += '_' + scene[10] + scene[14]
+    url = f'{SENTINEL_DISTRIBUTION_URL}/{product_type}/{mission}/{scene}.zip'
+    return url
