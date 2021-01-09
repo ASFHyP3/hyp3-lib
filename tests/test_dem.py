1b19955778c27440cc697429510dea95d78ed882
Andrew Johnston <asjohnston@alaska.edu>
2021-01-08 16:53:42 -0900

    add test_get_polygon_from_manifest

--- tests/test_dem.py
+++ tests/test_dem.py
@@ -2,28 +2,34 @@ import json
 
 import pytest
 
-from hyp3lib import DemError
-from hyp3lib.dem import get_best_dem, utm_from_lon_lat, get_coverage_geometry
+from hyp3lib import DemError, dem
 
 
 # def test_get_best_dem():
-#     assert get_best_dem(y_min=37.0, y_max=38.0, x_min=-118.0, x_max=-117.0) == 'COP30'
-#     assert get_best_dem(y_min=-6.8, y_max=-6.2, x_min=27.2, x_max=27.8) == 'SRTMGL1'
-#     assert get_best_dem(y_min=59.975, y_max=60.1, x_min=99.5, x_max=100.5) == 'COP30'
-#     assert get_best_dem(y_min=59.976, y_max=60.1, x_min=99.5, x_max=100.5) == 'COP30'
+#     assert dem.get_best_dem(y_min=37.0, y_max=38.0, x_min=-118.0, x_max=-117.0) == 'COP30'
+#     assert dem.get_best_dem(y_min=-6.8, y_max=-6.2, x_min=27.2, x_max=27.8) == 'SRTMGL1'
+#     assert dem.get_best_dem(y_min=59.975, y_max=60.1, x_min=99.5, x_max=100.5) == 'COP30'
+#     assert dem.get_best_dem(y_min=59.976, y_max=60.1, x_min=99.5, x_max=100.5) == 'COP30'
 #
 #     with pytest.raises(DemError):
-#         get_best_dem(y_min=0, y_max=1, x_min=0, x_max=1)
+#         dem.get_best_dem(y_min=0, y_max=1, x_min=0, x_max=1)
+
+
+def test_get_polygon_from_manifest(test_data_folder):
+    manifest_file = test_data_folder / 'test.SAFE' / 'manifest.safe'
+    polygon = dem.get_polygon_from_manifest(str(manifest_file))
+    assert polygon.ExportToWkt() == 'POLYGON ((-111.577644 37.063725,-114.392342 37.459259,-114.048729 39.136841,' \
+                                    '-111.166084 38.742691,-111.577644 37.063725))'
 
 
 def test_utm_from_lat_lon():
-    assert utm_from_lon_lat(0, 0) == 32631
-    assert utm_from_lon_lat(-179, -1) == 32701
-    assert utm_from_lon_lat(179, 1) == 32660
-    assert utm_from_lon_lat(27, 89) == 32635
-    assert utm_from_lon_lat(182, 1) == 32601
-    assert utm_from_lon_lat(-182, 1) == 32660
-    assert utm_from_lon_lat(-360, -1) == 32731
+    assert dem.utm_from_lon_lat(0, 0) == 32631
+    assert dem.utm_from_lon_lat(-179, -1) == 32701
+    assert dem.utm_from_lon_lat(179, 1) == 32660
+    assert dem.utm_from_lon_lat(27, 89) == 32635
+    assert dem.utm_from_lon_lat(182, 1) == 32601
+    assert dem.utm_from_lon_lat(-182, 1) == 32660
+    assert dem.utm_from_lon_lat(-360, -1) == 32731
 
 
 def test_get_coverage_geometry(tmp_path):
@@ -43,7 +49,7 @@ def test_get_coverage_geometry(tmp_path):
     with open(coverage_file, 'w') as f:
         json.dump(geojson, f)
 
-    geom = get_coverage_geometry(str(coverage_file))
+    geom = dem.get_coverage_geometry(str(coverage_file))
     assert geom.ExportToWkt() == 'POINT (125.6 10.1)'
 
 
