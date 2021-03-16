6e70077883f59b951d4ce1f540aa6ef96145a3cf
Andrew Johnston <asjohnston@alaska.edu>
2021-03-16 14:04:53 -0800

    update test_get_orb

--- tests/test_get_orb.py
+++ tests/test_get_orb.py
@@ -1,4 +1,5 @@
 import os
+from unittest.mock import patch
 
 import responses
 
@@ -8,47 +9,44 @@ _GRANULE = 'S1A_IW_SLC__1SSV_20150621T120220_20150621T120232_006471_008934_72D8'
 
 
 @responses.activate
-def test_download_sentinel_orbit_file_esa():
-    remote_url = 'http://aux.sentinel1.eo.esa.int/POEORB/2015/07/11/' \
-                 'S1A_OPER_AUX_POEORB_OPOD_20150711T121908_V20150620T225944_20150622T005944.EOF'
-    responses.add_passthru(remote_url)
+def test_download_sentinel_orbit_file_esa(tmp_path):
+    responses.add(responses.GET, 'https://foo.bar/hello.txt', body='content')
 
-    response = {'results': [{'remote_url': remote_url}]}
-    responses.add(responses.GET, 'https://qc.sentinel1.eo.esa.int/api/v1/', json=response)
-
-    orbit_file, provider = get_orb.downloadSentinelOrbitFile(_GRANULE, providers=('ESA',))
+    with patch('hyp3lib.get_orb.get_orbit_url', return_value='https://foo.bar/hello.txt'):
+        responses.add(responses.GET, 'https://foo.bar/hello.txt', body='content')
+        orbit_file, provider = get_orb.downloadSentinelOrbitFile(_GRANULE, providers=('ESA',), directory=str(tmp_path))
 
     assert provider == 'ESA'
     assert os.path.exists(orbit_file)
-    assert orbit_file == 'S1A_OPER_AUX_POEORB_OPOD_20150711T121908_V20150620T225944_20150622T005944.EOF'
+    assert orbit_file == str(tmp_path / 'hello.txt')
 
 
 @responses.activate
 def test_get_orbit_url_esa_poeorb():
-    remote_url = 'http://aux.sentinel1.eo.esa.int/POEORB/2015/07/11/' \
-                 'S1A_OPER_AUX_POEORB_OPOD_20150711T121908_V20150620T225944_20150622T005944.EOF'
-    response = {'results': [{'remote_url': remote_url}]}
-    responses.add(responses.GET, 'https://qc.sentinel1.eo.esa.int/api/v1/', json=response)
+    search_url = 'https://scihub.copernicus.eu/gnss/api/stub/products' \
+                 '?filter=%28platformname%3ASentinel-1+AND+producttype%3AAUX_POEORB+AND+filename%3AS1A%2A+' \
+                 'AND+beginPosition%3A%5B%2A+TO+2015-06-21T12%3A02%3A20Z%5D+' \
+                 'AND+endPosition%3A%5B2015-06-21T12%3A02%3A32Z+TO+NOW%5D%29' \
+                 '&limit=1&offset=0&sortedby=ingestiondate&order=desc'
+    search_response = {'products': [{'uuid': 'myUUID'}]}
+    responses.add(responses.GET, search_url, json=search_response)
 
     orbit_url = get_orb.get_orbit_url(_GRANULE, provider='ESA')
-
-    assert 'http://aux.sentinel1.eo.esa.int/POEORB/2015/07/11/' \
-           'S1A_OPER_AUX_POEORB_OPOD_20150711T121908_V20150620T225944_20150622T005944.EOF' \
-           == orbit_url
+    assert orbit_url == "https://scihub.copernicus.eu/gnss/odata/v1/Products('myUUID')/$value"
 
 
 @responses.activate
 def test_get_orbit_url_esa_resorb():
-    remote_url = 'http://aux.sentinel1.eo.esa.int/RESORB/2015/06/21/' \
-                 'S1A_OPER_AUX_RESORB_OPOD_20150621T152320_V20150621T111644_20150621T143414.EOF'
-    response = {'results': [{'remote_url': remote_url}]}
-    responses.add(responses.GET, 'https://qc.sentinel1.eo.esa.int/api/v1/', json=response)
+    search_url = 'https://scihub.copernicus.eu/gnss/api/stub/products' \
+                 '?filter=%28platformname%3ASentinel-1+AND+producttype%3AAUX_RESORB+AND+filename%3AS1A%2A+' \
+                 'AND+beginPosition%3A%5B%2A+TO+2015-06-21T12%3A02%3A32Z%5D+' \
+                 'AND+endPosition%3A%5B2015-06-21T12%3A02%3A20Z+TO+NOW%5D%29' \
+                 '&limit=1&offset=0&sortedby=ingestiondate&order=desc'
+    search_response = {'products': [{'uuid': 'myUUID'}]}
+    responses.add(responses.GET, search_url, json=search_response)
 
-    orbit_url = get_orb.get_orbit_url(_GRANULE, orbit_type='aux_resorb', provider='ESA')
-
-    assert 'http://aux.sentinel1.eo.esa.int/RESORB/2015/06/21/' \
-           'S1A_OPER_AUX_RESORB_OPOD_20150621T152320_V20150621T111644_20150621T143414.EOF' \
-           == orbit_url
+    orbit_url = get_orb.get_orbit_url(_GRANULE, provider='ESA', orbit_type='AUX_RESORB')
+    assert orbit_url == "https://scihub.copernicus.eu/gnss/odata/v1/Products('myUUID')/$value"
 
 
 def test_get_orbit_url_asf():
