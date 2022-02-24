267c6e2036b88b40b2997a7e2c568512112f7e6b
Andrew Johnston <asjohnston@alaska.edu>
2022-02-23 17:03:40 -0900

    remove python 3.6 support, add 3.10

--- setup.py
+++ setup.py
@@ -25,14 +25,14 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Topic :: Software Development :: Libraries',
-        ],
+    ],
 
-    python_requires='~=3.6',
+    python_requires='>=3.7',
 
     install_requires=[
         'boto3',
