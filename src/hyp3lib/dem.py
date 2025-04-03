2cfd3d98a6a2aef6e6604252a0c11d23c254fc96
Andrew Johnston <asjohnston@alaska.edu>
2025-04-03 13:12:07 -0800

    refactor hyp3lib.dem.prepare_dem_geotiff

--- src/hyp3lib/dem.py
+++ src/hyp3lib/dem.py
@@ -1,13 +1,11 @@
-import json
 from collections.abc import Generator
 from pathlib import Path
-from subprocess import PIPE, run
-from tempfile import TemporaryDirectory
+from tempfile import NamedTemporaryFile, TemporaryDirectory
 
-from osgeo import gdal, ogr, osr
+from osgeo import gdal, ogr
 
 from hyp3lib import DemError
-
+from hyp3lib.util import GDALConfigManager
 
 DEM_GEOJSON = '/vsicurl/https://asf-dem-west.s3.amazonaws.com/v2/cop30-2021-with-cop90-us-west-2-mirror.geojson'
 GEOID = '/vsicurl/https://asf-dem-west.s3.amazonaws.com/GEOID/us_nga_egm2008_1.tif'
@@ -16,47 +14,7 @@ gdal.UseExceptions()
 ogr.UseExceptions()
 
 
-class GDALConfigManager:
-    """Context manager for setting GDAL config options temporarily"""
-
-    def __init__(self, **options):
-        """Args:
-        **options: GDAL Config `option=value` keyword arguments.
-        """
-        self.options = options.copy()
-        self._previous_options = {}
-
-    def __enter__(self):
-        for key in self.options:
-            self._previous_options[key] = gdal.GetConfigOption(key)
-
-        for key, value in self.options.items():
-            gdal.SetConfigOption(key, value)
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        for key, value in self._previous_options.items():
-            gdal.SetConfigOption(key, value)
-
-
-def get_geometry_from_kml(kml_file: str) -> ogr.Geometry:
-    cmd = [
-        'ogr2ogr',
-        '-wrapdateline',
-        '-datelineoffset',
-        '20',
-        '-f',
-        'GeoJSON',
-        '-mapfieldtype',
-        'DateTime=String',
-        '/vsistdout',
-        kml_file,
-    ]
-    geojson_str = run(cmd, stdout=PIPE, check=True).stdout
-    geometry = json.loads(geojson_str)['features'][0]['geometry']
-    return ogr.CreateGeometryFromJson(json.dumps(geometry))
-
-
-def get_dem_features() -> Generator[ogr.Feature, None, None]:
+def _get_dem_features() -> Generator[ogr.Feature, None, None]:
     ds = ogr.Open(DEM_GEOJSON)
     layer = ds.GetLayer()
     for feature in layer:
@@ -64,38 +22,22 @@ def get_dem_features() -> Generator[ogr.Feature, None, None]:
     del ds
 
 
-def intersects_dem(geometry: ogr.Geometry) -> bool:
-    for feature in get_dem_features():
+def _intersects_dem(geometry: ogr.Geometry) -> bool:
+    for feature in _get_dem_features():
         if feature.GetGeometryRef().Intersects(geometry):
             return True
     return False
 
 
-def get_dem_file_paths(geometry: ogr.Geometry) -> list[str]:
+def _get_dem_file_paths(geometry: ogr.Geometry) -> list[str]:
     file_paths = []
-    for feature in get_dem_features():
+    for feature in _get_dem_features():
         if feature.GetGeometryRef().Intersects(geometry):
             file_paths.append(feature.GetField('file_path'))
     return file_paths
 
 
-def utm_from_lon_lat(lon: float, lat: float) -> int:
-    hemisphere = 32600 if lat >= 0 else 32700
-    zone = int(lon // 6 + 30) % 60 + 1
-    return hemisphere + zone
-
-
-def get_centroid_crossing_antimeridian(geometry: ogr.Geometry) -> ogr.Geometry:
-    geojson = json.loads(geometry.ExportToJson())
-    for feature in geojson['coordinates']:
-        for point in feature[0]:
-            if point[0] < 0:
-                point[0] += 360
-    shifted_geometry = ogr.CreateGeometryFromJson(json.dumps(geojson))
-    return shifted_geometry.Centroid()
-
-
-def shift_for_antimeridian(dem_file_paths: list[str], directory: Path) -> list[str]:
+def _shift_for_antimeridian(dem_file_paths: list[str], directory: Path) -> list[str]:
     shifted_file_paths = []
     for file_path in dem_file_paths:
         if '_W' in file_path:
@@ -114,80 +56,73 @@ def shift_for_antimeridian(dem_file_paths: list[str], directory: Path) -> list[s
     return shifted_file_paths
 
 
-def convert_to_hae(dem_path: str, geoid_path: str):
-    """Convert DEM to height above ellipsoid by adding geoid height.
+def _convert_to_height_above_ellipsoid(dem_file: Path) -> None:
+    dem_info = gdal.Info(str(dem_file), format='json')
+    with NamedTemporaryFile() as geoid_file:
+        gdal.Warp(
+            geoid_file.name,
+            GEOID,
+            dstSRS=dem_info['coordinateSystem']['wkt'],
+            outputBounds=[
+                dem_info['cornerCoordinates']['lowerLeft'][0],
+                dem_info['cornerCoordinates']['lowerLeft'][1],
+                dem_info['cornerCoordinates']['upperRight'][0],
+                dem_info['cornerCoordinates']['upperRight'][1],
+            ],
+            xRes=dem_info['geoTransform'][1],
+            yRes=-dem_info['geoTransform'][5],
+            targetAlignedPixels=True,
+            resampleAlg='cubic',
+            multithread=True,
+        )
+        geoid_ds = gdal.Open(geoid_file.name)
+        geoid_data = geoid_ds.GetRasterBand(1).ReadAsArray()
+        del geoid_ds
 
-    Args:
-        dem_path: Path to an existing the DEM
-        geoid_path: Path to save the geoid height GeoTIFF to
-    """
-    # FIXME: Doesn't supprot antimeridian crossing
-    dem_ds = gdal.Open(dem_path, gdal.GA_Update)
-    srs = osr.SpatialReference()
-    srs.ImportFromWkt(dem_ds.GetProjection())
-    epsg_code = srs.GetAuthorityCode(None)
-    geotransform = dem_ds.GetGeoTransform()
-    minx = geotransform[0]
-    maxx = geotransform[0] + geotransform[1] * dem_ds.RasterXSize
-    maxy = geotransform[3]
-    miny = geotransform[3] + geotransform[5] * dem_ds.RasterYSize
-    pixel_size = geotransform[1]
-    gdal.Warp(
-        str(geoid_path),
-        GEOID,
-        dstSRS=f'EPSG:{epsg_code}',
-        outputBounds=[minx, miny, maxx, maxy],
-        xRes=pixel_size,
-        yRes=pixel_size,
-        targetAlignedPixels=True,
-        resampleAlg='cubic',
-        multithread=True,
-    )
-    dem_data = dem_ds.GetRasterBand(1).ReadAsArray()
-    geoid_ds = gdal.Open(str(geoid_path))
-    geoid_data = geoid_ds.GetRasterBand(1).ReadAsArray()
-    dem_data += geoid_data
-    dem_ds.GetRasterBand(1).WriteArray(dem_data)
-    dem_ds.FlushCache()
-    del dem_ds, geoid_ds
+        dem_ds = gdal.Open(str(dem_file), gdal.GA_Update)
+        dem_data = dem_ds.GetRasterBand(1).ReadAsArray()
+        dem_data += geoid_data
+        dem_ds.GetRasterBand(1).WriteArray(dem_data)
+        dem_ds.FlushCache()
+        del dem_ds
 
 
 def prepare_dem_geotiff(
-    output_name: str,
+    output_name: Path,
     geometry: ogr.Geometry,
     pixel_size: float,
-    epsg_code: int = None,
+    epsg_code: int,
     height_above_ellipsoid: bool = False,
-):
+) -> Path:
     """Create a DEM mosaic GeoTIFF covering a given geometry.
 
-    The DEM mosaic is assembled from the Copernicus GLO-30 Public DEM. The output GeoTIFF covers the input geometry
-    buffered by 0.15 degrees, is projected to the UTM zone of the geometry centroid, and has a pixel size of 30m.
+    The DEM mosaic is assembled from the Copernicus GLO-30 Public DEM. The output GeoTIFF covers the extent of the input
+    geometry.
 
     Args:
         output_name: Path for the output GeoTIFF
         geometry: Geometry in EPSG:4326 (lon/lat) projection for which to prepare a DEM mosaic
+          MULTIPOLYGON geometries are assumed to cross the antimeridian with one polygon in each hemisphere.
         pixel_size: Pixel size for the DEM in units of the DEM's projection
-        epsg_code: EPSG code for the output GeoTIFF projection. If None, the UTM zone of the geometry centroid is used.
-        height_above_ellipsoid: If True, the output GeoTIFF will contain height above ellipsoid values.
-        format: Output GeoTIFF format
+        epsg_code: EPSG code for the output GeoTIFF projection.
+        height_above_ellipsoid:
+          If False, output pixel values will be meters above mean sea level.
+          If True, the output GeoTIFF will contain height above ellipsoid values.
     """
-    geometry = geometry.Buffer(0.15)
-    minx, maxx, miny, maxy = geometry.GetEnvelope()
-    if not intersects_dem(geometry):
+    if not _intersects_dem(geometry):
         raise DemError(f'Copernicus GLO-30 Public DEM does not intersect this geometry: {geometry}')
-
-    if epsg_code is None:
-        centroid = geometry.Centroid()
-        epsg_code = utm_from_lon_lat(centroid.GetX(), centroid.GetY())
 
     with GDALConfigManager(GDAL_DISABLE_READDIR_ON_OPEN='EMPTY_DIR'):
         with TemporaryDirectory() as temp_dir:
             temp_path = Path(temp_dir)
-            dem_file_paths = get_dem_file_paths(geometry)
+            dem_file_paths = _get_dem_file_paths(geometry)
+
             if geometry.GetGeometryName() == 'MULTIPOLYGON':
-                centroid = get_centroid_crossing_antimeridian(geometry)
-                dem_file_paths = shift_for_antimeridian(dem_file_paths, temp_path)
+                if height_above_ellipsoid:
+                    raise DemError('height_above_ellipsoid is not supported for geometries crossing the antimeridian.')
+                dem_file_paths = _shift_for_antimeridian(dem_file_paths, temp_path)
+
+            minx, maxx, miny, maxy = geometry.GetEnvelope()
 
             dem_vrt = temp_path / 'dem.vrt'
             gdal.BuildVRT(str(dem_vrt), dem_file_paths)
@@ -195,6 +130,7 @@ def prepare_dem_geotiff(
                 str(output_name),
                 str(dem_vrt),
                 dstSRS=f'EPSG:{epsg_code}',
+                outputBoundsSRS='EPSG:4326',
                 outputBounds=[minx, miny, maxx, maxy],
                 xRes=pixel_size,
                 yRes=pixel_size,
@@ -204,24 +140,7 @@ def prepare_dem_geotiff(
                 format='GTiff',
             )
 
-            if height_above_ellipsoid:
-                geoid_path = temp_path / 'geoid.tif'
-                gdal.Warp(
-                    str(geoid_path),
-                    GEOID,
-                    dstSRS=f'EPSG:{epsg_code}',
-                    outputBounds=[minx, miny, maxx, maxy],
-                    xRes=pixel_size,
-                    yRes=pixel_size,
-                    targetAlignedPixels=True,
-                    resampleAlg='cubic',
-                    multithread=True,
-                )
-                dem_ds = gdal.Open(output_name, gdal.GA_Update)
-                dem_data = dem_ds.GetRasterBand(1).ReadAsArray()
-                geoid_ds = gdal.Open(str(geoid_path))
-                geoid_data = geoid_ds.GetRasterBand(1).ReadAsArray()
-                dem_data += geoid_data
-                dem_ds.GetRasterBand(1).WriteArray(dem_data)
-                dem_ds.FlushCache()
-                del dem_ds, geoid_ds
+    if height_above_ellipsoid:
+        _convert_to_height_above_ellipsoid(output_name)
+
+    return output_name
